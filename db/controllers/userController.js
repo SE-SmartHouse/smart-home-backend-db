904bb29668bff178c0cb125b53f0fb768679bc53
Shahzaib Khan <shahzaib.khan0014@stud.hkr.se>
2025-04-03 22:35:54 +0200

    Updated features -Link user with house, and house with user,userid in resposne to login

--- db/controllers/userController.js
+++ db/controllers/userController.js
@@ -64,7 +64,10 @@ const loginUser = async (req, res) => {
             return res.status(401).json({ message: 'Invalid credentials.' });
         }
         // Respond with a success message and token
-        return res.status(200).json({ message: 'User logged in'});
+        return res.status(200).json({
+            message: 'User logged in',
+            userId: user._id.toString(), // Convert ObjectId to string
+        });
     } catch (error) {
         console.error('Error during login:', error);
         return res.status(500).json({ message: 'An error occurred during login.', error: error.message });
